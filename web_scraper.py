e59efb567a62c8ed2084c7872c1aa650a5526cb5
kaw0394 <kaw0394@unt.edu>
2021-10-26 19:19:05 -0500

    put web_scraper into function

--- web_scraper.py
+++ web_scraper.py
@@ -3,15 +3,17 @@ import urllib.request
 from bs4 import BeautifulSoup
 import nltk
 
-# stored web page value
-webpage = 'https://www.ibm.com/cloud/learn/natural-language-processing'
+def web_scraper():
+    # stored web page value
+    webpage = 'https://www.ibm.com/cloud/learn/natural-language-processing'
 
-# get data from web pages
-response = urllib.request.urlopen(webpage)
-html = response.read()
+    # get data from web pages
+    response = urllib.request.urlopen(webpage)
+    html = response.read()
 
-# use beautiful soup and html5lib to clean out the html
-soup = BeautifulSoup(html,"html5lib")
-text = soup.find('article').get_text(strip=True)
+    # use beautiful soup and html5lib to clean out the html
+    soup = BeautifulSoup(html,"html5lib")
+    text = soup.find('article').get_text(strip=True)
 
-print(f"{text}")
+    print(f"{text}")
+
