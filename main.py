8d29db47eb34d22d266f2a19d81eb057ae020659
Kaitlynn Whitney <80166708+glitchQween@users.noreply.github.com>
2021-12-02 17:46:02 -0600

    added failed to find answer response
    added failed to find answer response

--- main.py
+++ main.py
@@ -86,7 +86,9 @@ def answer(question, text):
         if not result or prediciton[1] >= result["probability"]:
             result["answer"] = prediciton[0]
             result["probability"] = prediciton[1]
-
+    if result["probability"] < 1:
+        result["answer"] = "No answer was found for this question."
+    
     return(result["answer"])
 
 
